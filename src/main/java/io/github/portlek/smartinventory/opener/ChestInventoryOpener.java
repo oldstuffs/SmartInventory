16f50461af7f75ff75a9d9f7a4e382a0ef8e0b8f
Hasan Demirtaş <utsukushihito@outlook.com>
2021-02-21 15:01:50 +0300

    2021

--- src/main/java/io/github/portlek/smartinventory/opener/ChestInventoryOpener.java
+++ src/main/java/io/github/portlek/smartinventory/opener/ChestInventoryOpener.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2020 Hasan Demirtaş
+ * Copyright (c) 2021 Hasan Demirtaş
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
